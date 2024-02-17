4a642aafe526b8cb7e52a991aa48f5057dfa7a1c
LENOVO <LENOVO@LENOVO-PC>
2024-02-17 18:04:48 +0530

    Commit

---
+++ IntroductionToJava/src/com/capgemini/introduction/HelloWorldDemo.java
@@ -0,0 +1,10 @@
+package com.capgemini.introduction;
+
+public class HelloWorldDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Hey, There I Am Writing Hello World Program");
+
+	}
+
+}
